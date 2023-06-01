74c4a199170f4d8980810792bd690d591d5526e9
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2023-06-01 19:57:18 +0530

    Add files via upload

---
+++ variables.ts
@@ -0,0 +1,14 @@
+let userId : number = 33445.4
+let n = userId.toPrecision(3345.4)
+console.log(n)
+
+//any
+let hero;   //the type value is not defined , so in this case it should be any value
+
+function getHero(){
+    return "thor";
+}
+
+hero = getHero();
+
+export {}
